d65b795e1916af7edadb2abda062802663297ac7
Sagnik Saha <sahasagnik279@gmail.com>
2026-01-14 11:37:08 +0530

    Update passport.js

--- utils/passport.js
+++ utils/passport.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const db = require("./db");
 
-const server = "http://e-commerce-279.vercel.app";
+const server = "https://e-commerce-279.vercel.app";
 
 passport.use(
     new GoogleStrategy(
